5bf21a745438ac184a058522932d50f859a6bcb1
Esso-Ham <esspohoyi@gmail.com>
2022-02-15 17:32:25 +0100

    Ajouter du voyage dans le temps : Implémenter le voyage dans le temps.

--- src/index.js
+++ src/index.js
@@ -54,12 +54,13 @@ class Game extends React.Component {
                 squares : Array(9).fill(null),
             }],            
             xIsNext: true,
+            stepNumber: 0,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length - 1];
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[this.state.stepNumber];
         //console.log(history, current);
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -71,6 +72,14 @@ class Game extends React.Component {
                 squares: squares,
             }]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
